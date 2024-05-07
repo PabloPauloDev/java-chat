b92d2bce9d7af94a28f23d16bc3e44b2cafa75fe
Pablo Carpanedo <pablopaulodev@gmail.com>
2024-05-07 19:47:48 -0300

    Configurar metodos de alterar, sair e entra

--- Server/src/main/Server.java
+++ Server/src/main/Server.java
@@ -11,19 +11,49 @@ import java.util.ArrayList;
 public class Server implements Runnable {
 
     private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private done;
+
+    public server() {
+        connections = new ArrayList<>();
+        done = false;
+    }
 
     public void run(){
 
         try {
-            ServerSocket server = new ServerSocket(9999);
-            Socket client = server.accept();
-            ConnectionHandler handler = new ConnectionHandler(client);
-
-            connections.add(handler);
+            server = new ServerSocket(9999);
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+            }
         } 
         catch (IOException e){
 
             e.printStackTrace();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (connectionHandler ch: connections) {
+            if (ch != null) {
+                ch.sendMessage(message);
+            }
+        }
+    }
+
+    public void shutdown(){
+        try {
+            done = true
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (connectionHandler ch : connections) {
+                ch.shutdown();
+            }
+        } catch (IOException e) {
+
         }
     }
     /**
@@ -48,11 +78,33 @@ public class Server implements Runnable {
                 out.println("Por favor insira seu nome de usuario:");
                 usuario = in.readLine();
                 System.out.println(usuario + "Conectado!");
+                broadcast(usuario + "entrou no chat!");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/usuario")){
+                        String[] messageSplit = message.split("", 2);
+                        if (messageSplit.length == 2){
+                            broadcast(usuario + "alterou seu nome para:" + messageSplit[1]);
+                            System.out.println(usuario + "alterou seu nome para:" + messageSplit[1])
+                            usuario = messageSplit[1]
+                        } else {
+                            out.println("Nome de usuario nao solicitado")
+                        }
+                    } else if (message.startsWith("/quit")) {
+
+                    }else {
+                        broadcast(usuario + ":  " + message)
+                    }
+                } 
             } 
             catch (IOException e){
     
                 e.printStackTrace();
             }
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
         }
     }
 }
